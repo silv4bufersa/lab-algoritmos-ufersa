265c1a5b48395d4bf0f0e14175bf1ad892dd32ba
Bruno Silva (silv4b) <brunno.linkin@gmail.com>
2026-04-16 02:18:11 -0300

    code/doc: atualização do readme.md e correção em 1-exemplos-variaveis.c

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # lab-algoritmos-ufersa
+
 Repositório com códigos referentes as disciplinas de Algoritmos e Laboratório de Algoritmos
+
+## Pastas
+
+`aulas/`: Arquivos das aulas de programação ministradas em sala. (a partir da segunda unidade).
+
+`exemplos/`: Todos os exemplos que vemos em sala e que por ventura venham a ser adicionados para consulta ou estudos.
+
+`listas/`: Listas disponibilizadas no SIGAA e suas resoluções.
+
+* Resoluções das listas publicadas após o prazo de entrega ou resolvidas em sala.
+
+`manual-c/`: Manual de consulta primária ao material, contendo exemplos de códigos e definições.
--- exemplos/1-exemplos-aula-14-04-2026/1-exemplo-variaveis.c
+++ exemplos/1-exemplos-aula-14-04-2026/1-exemplo-variaveis.c
@@ -1,23 +1,38 @@
 #include <stdio.h>
+#include <string.h>  // para lidar com strings (spoiler)
+#include <stdbool.h> // para usar bool ao invés de _Bool
+
 int main(void)
 {
     int inteiro;
     float ponto_flutante;
     char letra;
+    char nome[7];
     double duas_casas;
-    _Bool boleano; // 1 ou 0
+    _Bool boleano_nativo; // 1 ou 0 -> boleanos de 1 (verdadeiro) e 0 (falso)
+    bool boleano_moderno;
 
     inteiro = 10;
     ponto_flutante = 3.1;
     letra = 'b';
     duas_casas = 1.852;
-    boleano = 1;
+    boleano_nativo = 1;
+    boleano_moderno = false;
+    // forma de correta de atribuir strings a um vetor de caracteres
+    strcpy(nome, "fulano");
 
     printf("int: %d\n", inteiro);
     printf("float: %.2f\n", ponto_flutante);
     printf("double: %.2f\n", duas_casas);
     printf("char: '%c'\n", letra);
-    printf("boolean: %d", boleano);
+    printf("chars: '%s'\n", nome);
+
+    // ambas as formas (nativa e moderna) são printadas como 1 ou 0 (%d)
+    printf("_Bool (nativo): %d\n", boleano_nativo);
+    printf("bool (moderno): %d\n", boleano_moderno);
+
+    // Para exibir a palavra, você precisa usar um operador ternário
+    printf("bool (moderno): %s\n", boleano_moderno ? "true" : "false");
 
     return 0;
 }
