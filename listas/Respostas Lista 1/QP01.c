068639d0400421cd9b6d2a12d023b51c13ab5df2
AliranTi <antoniojoaquim267@gmail.com>
2026-04-20 22:09:54 -0300

    Questoes respondidas 1-5 | alterado o gitignore para conter *.exe

---
+++ listas/Respostas Lista 1/QP01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    // declarando as variáveis para os pesos já com os valores
+    int peso1 =5;
+    int peso2 =10;
+    int peso3 =15;
+
+    // somando e imprimindo o peso total (usando eh pois o meu terminal nao aceita os acentos)
+    printf("O peso total eh: %d\n", peso1+peso2+peso3);
+    return 0;
+}
