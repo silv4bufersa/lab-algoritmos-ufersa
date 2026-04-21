f27861122b9b44acf2988bb34098dc3f9e5aeaaf
AliranTi <antoniojoaquim267@gmail.com>
2026-04-20 22:19:45 -0300

    Questoes respondidas 1-5

---
+++ listas/Respostas Lista 1/QP04.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int quantidadeDeDias = 10;
+
+    int quantidadeDeHoras = quantidadeDeDias * 24;
+
+    printf("A quantidade de horas em %d dias eh: %d\n", quantidadeDeDias, quantidadeDeHoras);
+    return 0;
+}
